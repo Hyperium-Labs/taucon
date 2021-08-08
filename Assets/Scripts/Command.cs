4c3a0e2e3225e31893d84e013ad24d16389e5e7f
Trevor <ybvQmBgKGHG9@protonmail.com>
2021-08-07 22:22:24 -0400

    restructure project folders

---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Taucon
+{
+    /// <summary>
+    /// Constructor class for TauCon commands.
+    /// </summary>
+    public class Command
+    {
+        public string name;
+        public string command;
+        public string description;
+        public Func<string, string> method;
+        public string helpText;
+
+        public Command(string name, string command, string description, Func<string, string> method, string helpText = null)
+        {
+            this.name = name;
+            this.command = command;
+            this.description = description;
+            this.method = method;
+            this.helpText = helpText;
+        }
+    }
+}
+
