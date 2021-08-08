266f73db54b81b34d450e8c7350ddabd459d0aaf
Trevor <ybvQmBgKGHG9@protonmail.com>
2021-08-08 14:44:29 -0400

    added commands command
    - added 'commands' command to list all available commands

---
+++ Assets/Scripts/Commands/Commands.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Console.Cmd
+{
+
+    /// <summary>
+    /// Lists all available commands
+    /// </summary>
+    public class Commands : ScriptableObject
+    {
+
+        private void Awake()
+        {
+            this.hideFlags = HideFlags.HideAndDontSave;
+            
+        }
+
+        public static string ListCommands(string param)
+        {
+            string result = string.Empty;
+
+            foreach (string command in TauCon.Commands.Keys)
+            {
+                result += command + "\t";
+            }
+            return result;
+        }
+    }
+}
